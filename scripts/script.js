22a59c17d85217fbc38350754a5221783c8cb0e5
Michael, Ze-Ren Chen <tsejen.tw@gmail.com>
2019-03-27 09:38:31 +1030

    add hit handler basic functionalities..

--- scripts/script.js
+++ scripts/script.js
@@ -4,52 +4,86 @@ const cardIcons = Object.freeze({0:`♠`, 1:`♥`, 2:`♣`, 3:`♦`});
 const cardColors = Object.freeze({0:`black`, 1:`red`, 2:`black`, 3:`red`});
 const cardNum2Char = Object.freeze({1:`Ace`, 2:`2`, 3:`3`, 4:`4`, 5:`5`, 6:`6`, 7:`7`, 8:`8`, 9:`9`, 10:`10`, 11:`Jack`, 12:`QQ`, 13:`K`});
 
+const GAME_PLAYER_PLAY = 0, 
+      GAME_DEALER_PLAY=1, 
+      GAME_P_WINS=2, 
+      GAME_DEALER_WINS=3,
+      GAME_WELCOME=4,
+      GAME_RESET=5;
+
+let state = GAME_WELCOME;
 const deck = new Deck();
 const dealer = new Hand();
 const player = new Hand();
+let hitButton, standButton, newGameButton;
 
 $( document ).ready(function() {
     dealer.setElements(document.querySelector(`.dealer-cards .deck`));
     player.setElements(document.querySelector(`.your-cards .deck`));
-    resetGame();
     
+    hitButton = document.querySelector(`#action-hit`);
+    standButton = document.querySelector(`#action-stand`);
+    newGameButton = document.querySelector(`#action-new-game`);
+    
+    newGameButton.addEventListener(`click`, newGameHandler);
+    hitButton.addEventListener(`click`, hitHandler);
+    standButton.addEventListener(`click`, standHandler);
+    state = GAME_WELCOME;
+    updateView();
 });
 
-//return t othe starter state
-function resetGame() {
-    dealer.viewPoos();
-    player.viewPoos();
-    // document.querySelectorAll('.card').forEach(e=>{;
-    //     let num = e.querySelector('div');
-    //     num.innerHTML = `💩`;
-    //     // num.style.color = `transparent`
-    //     // num.style['text-shadow'] = `0 0 0 red`
-    //     let color = e.querySelector('span');
-    //     // color.innerHTML = cardIcons[HEARTS];
-    //     // color.style.color = cardColors[HEARTS];
-    // });
 
+function hitHandler() {
+    console.log(`hit!`)
+    player.getCards(deck.getCards(1));
+    let score = player.getPoint();
+    updateView();
+}
+
+//return t othe starter state
+function newGameHandler() {
+    // dealer.getCards(deck.getCards(2))
+    state = GAME_RESET;
+    player.viewPoos();
+    dealer.viewPoos();
     
     deck.newDeck();
     deck.deckShuffle();
-    dealer.getCards(deck.getCards(2))
-    dealer.getPoint();
-    player.getCards(deck.getCards(2))
-    player.getPoint();
-    updateHTML()
-    //enable player interface
-    
+    player.emptyHand();
+    dealer.emptyHand();
+
+    player.getCards(deck.getCards(2));
+    hitButton.removeAttribute(`disabled`);
+    standButton.removeAttribute(`disabled`);
+    state = GAME_PLAYER_PLAY;
+    updateView();
+}
+function standHandler() {
+    console.log(`stand!`)
 }
 
-function updateHTML() {
+
+function viewCards() {
     dealer.viewCards();
     player.viewCards();
-    let hitbutton = document.querySelector('#action-hit');
-    hitbutton.removeAttribute('disabled');
-    hitbutton.addEventListener('click', playerHit);
-    let standbutton = document.querySelector('#action-stand');
-    standbutton.removeAttribute('disabled');
-    standbutton.addEventListener('click', playerStand);
+}
+
+function updateView() {
+    switch(state) {
+        case GAME_RESET:
+            newGame();
+            break;
+        case GAME_WELCOME:
+            dealer.viewPoos();
+            player.viewPoos();
+            break;
+        case GAME_PLAYER_PLAY:
+            viewCards();
+            break;
+        default:
+            viewCards()
+            break;
+    }
 }
 function playerHit() {
     console.log(`hit!`);
@@ -101,6 +135,9 @@ function Hand() {
     this.hand = [];
     this.elements = null;
 }
+Hand.prototype.emptyHand = function() {
+    this.hand.length = 0;
+}
 Hand.prototype.addCard = function (c) {
     this.cards.push(c);
 } 
@@ -113,18 +150,18 @@ Hand.prototype.setElements = function(elements) {
 };
 Hand.prototype.viewPoos = function() {
     if(this.elements) {
-        this.elements.querySelectorAll('div', e=>e.innerHTML = `💩`);
-        this.elements.querySelectorAll('span', e=>e.innerHTML = null);
+        this.elements.querySelectorAll('.card>div').forEach(e=>e.innerHTML = `💩`);
+        this.elements.querySelectorAll('span').forEach(e=>e.innerHTML = null);
     }
 }
 Hand.prototype.viewCards = function () {
-    for(let i=0; i<this.hand.length; i++) {
+    for(let i=0; i<this.hand.length && i<5; i++) {
         console.log(this.elements)
-        let face = this.elements.querySelector(`#Card${i+1} div`);
+        let face = this.elements.querySelector(`#Card${i+1}>div`);
         console.log(face);
         face.innerHTML = cardNum2Char[this.hand[i].num];
         face.style.color = cardColors[this.hand[i].color];
-        let color = this.elements.querySelector(`#Card${i+1} span`);
+        let color = this.elements.querySelector(`#Card${i+1}>span`);
         color.innerHTML = cardIcons[this.hand[i].color];
         color.style.color = cardColors[this.hand[i].color];
     }
